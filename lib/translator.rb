883359d1c54c1a9bebc6378059f4ebd3da3022e9
David Jaworski <jaws360@gmail.com>
2019-10-27 16:08:03 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,11 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(file_path)
+ YAML.load_file(file_path)
+ hash = {"get_meaning": {"angel", "angry", "bored", "confused", "embarrassed", "fish"
+ , "glasses", "grinning", "happy", "kiss", "sad", "surprised", "wink"}, "get_emoticon": {}}
+ return hash
 end
 
 def get_japanese_emoticon
@@ -11,3 +15,5 @@ end
 def get_english_meaning
   # code goes here
 end
+
+load_library("./lib/emoticons.yml")
